22a155e5b87b74a1f34c68f8713da9b6560cb139
mika <besson.mikael04@gmail.com>
2021-10-06 14:59:08 +0200

    changement de couleur des label et appel du fichier Json.data

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+
+    /*Animation du titre h1*/
+
 let title = document.querySelector("h1");
 let x = -1000;
 
 setInterval(()=>{
-    title.style.transform = "translateX("+x+"px)";
-    x+=4;
-    if(x===1000){
-        x=-1000;
-    }
-},1)
+
+        title.style.transform = "translateX("+x+"px)";
+        x+=2;
+        if(x===1000){
+            x=-1000;
+        }
+    },1)
+
+    /*Animation des titres h2*/
 
 let titles = document.getElementsByClassName("anim");
 let x2 = -300;
@@ -16,7 +22,7 @@ setInterval(()=>{
     for(let i=0; i < titles.length; i++){
         titles[i].style.transform = "translateX("+x2+"px)";
 
-        x2 +=1.5;
+        x2 +=1;
         if(x2>50){
             x2=50;
             titles[i].style.color = "blue";
@@ -24,14 +30,117 @@ setInterval(()=>{
     }
 },1)
 
-let colorChange = document.getElementsByTagName("label");
-let change = colorChange.innerHTML;
+    /*Cache des balises section*/
 
-    for(let i=0; i>colorChange.length; i++){
-        change = document.createElement("span");
-        colorChange.document.appendChild(change);
+let sect = document.querySelectorAll("section");
+let tog1 = document.querySelectorAll(".roll");
 
+sect.forEach(function(e){
+    e.style.display = "none";
+});
+
+tog1.forEach(function(e){
+    e.addEventListener("click",function (){
+        if(e.parentElement.nextElementSibling.style.display === "none"){
+            e.parentElement.nextElementSibling.style.display = "block";
+        }
+        else{
+            e.parentElement.nextElementSibling.style.display = "none";
+        }
+    })
+})
+
+    /*changement de couleur des balise "label" au survol de la souris*/
+
+let labs = document.getElementsByTagName("label");
+
+for(let label of labs){
+
+    let letter = label.innerHTML.split("");
+    label.innerHTML = "";
+
+    for(let x=0; x<letter.length; x++){
+        let span = document.createElement("span");
+        span.innerHTML = letter[x];
+        label.appendChild(span);
     }
+
+    label.addEventListener('mouseover', function (){
+        let letterTab = label.getElementsByTagName("span");
+        let interval = (x) => {
+            setTimeout(()=> {
+                letterTab[x].style.color = 'red';
+                letterTab[x].style.fontStyle ='italic';
+                x++;
+                if(x < letterTab.length){
+                    interval(x);}
+                },50);
+            }
+            interval(0);
+    })
+    label.addEventListener('mouseout', function (){
+        let letterTab = label.getElementsByTagName("span");
+        let interval = (x) => {
+            setTimeout(()=> {
+                letterTab[x].style.color = '';
+                letterTab[x].style.fontStyle ='';
+                x++;
+                if(x < letterTab.length){
+                    interval(x);}
+            },50);
+        }
+        interval(0);
+    })
+}
+
+    /*Recuperation des info Json*/
+let comp = document.querySelector(".comp");
+
+fetch("/data.json").then(resp => resp.json())
+    .then(json => {
+        for(let x of json["Experiences"]){
+            let li = document.createElement("li");
+            li.innerHTML = x;
+            comp.append(li);
+        }
+    })
+
+let hob = document.querySelector(".hob");
+
+fetch("/data.json").then(resp => resp.json())
+    .then(json => {
+        for(let x of json["Mes hobbies"]){
+            let li = document.createElement("li");
+            li.innerHTML = x;
+            hob.append(li);
+        }
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
